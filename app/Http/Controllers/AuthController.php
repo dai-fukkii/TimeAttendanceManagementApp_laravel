fd4773f95709daef5106b7bdee4d458faf2e7520
河西大地 <kasaidaichi@kasaidaichinoMacBook-Pro.local>
2024-10-13 02:02:44 +0900

    create validation and login controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\user_info;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -18,13 +19,34 @@ class AuthController extends Controller
 
 
             user_info::create([
-                'username' => $request->username,
-                'password' => bcrypt($request->password),
+                'username' => $validationData['username'],
+                'password' => bcrypt($validationData['password']),
                 ]);
 
             return redirect()->route('login')->with('success','ユーザが作成されました');
         }
 
         return view('signup');
+    }
+
+    public function login(Request $request){
+
+        if ($request->isMethod('post')) {
+
+            $validationData = $request->validate([
+                'username' => 'required|string|max:255|min:6',
+                'password' => 'required|string|min:6',
+            ]);
+
+            if (Auth::attempt(['username'=>$validationData['username'], 'password'=>$validationData['password']])) {
+                return redirect()->intended('mytop')->with('success','ログインに成功しました');
+            }else {
+                return redirect()->back()->withErrors(['login_error'=>'ユーザ名かパスワードが間違っています'])->withInput();
+            }
+
+        }
+
+        return view('login');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::match(['get', 'post'],'/signup', [AuthController::class, 'signup']
 )->name('signup');
 //[AuthController::class ,'signup']
 
-Route::match(['get', 'post'],'/login', function() {
-    return view('login');
-})->name('login');
+Route::match(['get', 'post'],'/login', [AuthController::class, 'login']
+)->name('login');
+
+Route::match(['get','post'], '/mytop', function () {
+    return view('mytop');
+})->name('mytop');
